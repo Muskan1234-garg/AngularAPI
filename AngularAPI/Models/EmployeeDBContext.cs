448d5f5d683b1a31c757088b0a0402411861ce91
Muskan Garg <muskan.garg@dxc.com>
2020-10-27 14:54:53 +0530

    Add project files.

---
+++ AngularAPI/Models/EmployeeDBContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AngularAPI.Models
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server= IN-PF26QNRQ; Database=EmployeeDB;Persist Security Info=True;User ID=sa;Password=Muskan123 ");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.DepartmentId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.DepartmentName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.DateOfJoining).HasColumnType("date");
+
+                entity.Property(e => e.Department)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmployeeId).ValueGeneratedOnAdd();
+
+                entity.Property(e => e.EmployeeName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhotoFileName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
